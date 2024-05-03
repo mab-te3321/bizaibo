6cd902cf50520d57a06587181fa3847bc945d65c
Rashid Ali <rashid.ali000@gmail.com>
2024-05-03 16:54:43 +0500

    Dockerized

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -1,11 +1,11 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
 
-class DashboardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dashboard'
+# class DashboardConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'dashboard'
 
-    def ready(self):
-        from . import data  # Import your data module
-        data.create_builtin_integrations()
+#     def ready(self):
+#         from . import data  # Import your data module
+#         data.create_builtin_integrations()
 
